9a4a846957570cd1be2509a0f1797df820cc19e0
Omidreza <omidreza.shahbazi.web@gmail.com>
2024-10-04 22:40:08 +0330

    add css code to button

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ window.onload = function () {
     const open = document.getElementById('open')
     const close = document.getElementById('close')
     open.addEventListener('click', function () {
-        document.body.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        // document.body.style.backgroundColor = 'rgba(0,0,0,0.5)';
         open.style.display= 'none'
         popup.classList.add('active')
     })
 
     close.addEventListener('click', function () {
-        document.body.style.backgroundColor = 'aliceblue';
+        // document.body.style.backgroundColor = 'aliceblue';
         open.style.display= 'block'
         popup.classList.remove('active')
     })
